afba6f93b858027d855b5f2152021c06bd6f6310
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2023-07-28 17:49:21 +0530

    Add a /healthz path

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	// "github.com/go-chi/chi"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -22,6 +23,24 @@ func main() {
 	}
 
 	router := chi.NewRouter()
+	v1Router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	router.Mount("/v1", v1Router)
+
+	// path for the below is /v1/ready
+	v1Router.Get("/healthz", handlerReadiness)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
